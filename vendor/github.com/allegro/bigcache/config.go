d3eab6dad060e6579201c97082c7587cac885134
adhityaramadhanus <adhitya.ramadhanus@gmail.com>
2019-08-31 01:33:13 +0700

    commit vendor

--- vendor/github.com/allegro/bigcache/config.go
+++ vendor/github.com/allegro/bigcache/config.go
@@ -8,6 +8,9 @@ type Config struct {
 	Shards int
 	// Time after which entry can be evicted
 	LifeWindow time.Duration
+	// Interval between removing expired entries (clean up).
+	// If set to <= 0 then no action is performed. Setting to < 1 second is counterproductive — bigcache has a one second resolution.
+	CleanWindow time.Duration
 	// Max number of entries in life window. Used only to calculate initial size for cache shards.
 	// When proper value is set then additional memory allocation does not occur.
 	MaxEntriesInWindow int
@@ -23,8 +26,20 @@ type Config struct {
 	// the oldest entries are overridden for the new ones.
 	HardMaxCacheSize int
 	// OnRemove is a callback fired when the oldest entry is removed because of its expiration time or no space left
-	// for the new entry. Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+	// for the new entry, or because delete was called.
+	// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
 	OnRemove func(key string, entry []byte)
+	// OnRemoveWithReason is a callback fired when the oldest entry is removed because of its expiration time or no space left
+	// for the new entry, or because delete was called. A constant representing the reason will be passed through.
+	// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+	// Ignored if OnRemove is specified.
+	OnRemoveWithReason func(key string, entry []byte, reason RemoveReason)
+
+	onRemoveFilter int
+
+	// Logger is a logging interface and used in combination with `Verbose`
+	// Defaults to `DefaultLogger()`
+	Logger Logger
 }
 
 // DefaultConfig initializes config with default values.
@@ -33,11 +48,13 @@ func DefaultConfig(eviction time.Duration) Config {
 	return Config{
 		Shards:             1024,
 		LifeWindow:         eviction,
+		CleanWindow:        0,
 		MaxEntriesInWindow: 1000 * 10 * 60,
 		MaxEntrySize:       500,
 		Verbose:            true,
 		Hasher:             newDefaultHasher(),
 		HardMaxCacheSize:   0,
+		Logger:             DefaultLogger(),
 	}
 }
 
@@ -55,4 +72,15 @@ func (c Config) maximumShardSize() int {
 	}
 
 	return maxShardSize
+}
+
+// OnRemoveFilterSet sets which remove reasons will trigger a call to OnRemoveWithReason.
+// Filtering out reasons prevents bigcache from unwrapping them, which saves cpu.
+func (c Config) OnRemoveFilterSet(reasons ...RemoveReason) Config {
+	c.onRemoveFilter = 0
+	for i := range reasons {
+		c.onRemoveFilter |= 1 << uint(reasons[i])
+	}
+
+	return c
 }
